3be9a4975527487fa4e5e1faae7220f568747c23
Naveen Kushwaha <nkushwaha632@gmail.com>
2024-12-19 22:16:27 +0530

    sqlite database

--- library/settings.py
+++ library/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'library',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- myapp/models.py
+++ myapp/models.py
@@ -11,24 +11,24 @@ class User(models.Model):
         
 
 class Book(models.Model):
-    b_date = models.DateField()
-    b_category = models.CharField(max_length=50)
-    b_name = models.CharField(max_length=50)
-    b_author = models.CharField(max_length=20)
-    b_publish = models.CharField(max_length=50)
-    b_year = models.IntegerField()
+    b_date = models.DateField(null=True, blank=True)
+    b_category = models.CharField(max_length=50, null=True, blank=True)
+    b_name = models.CharField(max_length=50, null=True, blank=True)
+    b_author = models.CharField(max_length=20, null=True, blank=True)
+    b_publish = models.CharField(max_length=50, null=True, blank=True)
+    b_year = models.IntegerField(null=True, blank=True)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     class Meta:
 	    db_table='book'
   
   
 class Rental(models.Model):
-    r_date = models.DateField()
-    r_time = models.TimeField()
-    person_name = models.CharField(max_length=50)
-    rb_name = models.CharField(max_length=20)
-    return_date = models.DateField()  
-    submit = models.CharField(max_length=5)
+    r_date = models.DateField(null=True, blank=True)
+    r_time = models.TimeField(null=True, blank=True)
+    person_name = models.CharField(max_length=50, null=True, blank=True)
+    rb_name = models.CharField(max_length=20, null=True, blank=True)
+    return_date = models.DateField(null=True, blank=True)  
+    submit = models.CharField(max_length=5, null=True, blank=True)
     user=models.ForeignKey(User,on_delete=models.CASCADE) 
     class Meta:
 	    db_table='rental'
